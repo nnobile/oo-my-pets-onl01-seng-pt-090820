ed1ff23942be4349530fdaac0fa75977349fc099
Nick Nobile <nickcnobile@gmail.com>
2020-10-09 03:07:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,10 +57,10 @@ end
   def sell_pets
     Cat.all.map do |cat|
       cat.mood = "nervous"
-      cat.owner = ""
+      cat.owner = []
       Dog.all.map do |dog|
       dog.mood = "nervous"
-      dog.owner = ""
+      dog.owner = []
     end
   end
 
